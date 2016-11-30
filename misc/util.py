d0fbd166f1e36cb46c83b78827d06075b082a75d
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-11-30 13:22:21 +0100

    read youtube data and create snapshots

--- misc/util.py
+++ misc/util.py
@@ -1,5 +1,6 @@
 import json
-
+import os
+from pprint import pprint
 
 class SongSnapshot:
     def __init__(self, name, views, likes, dislikes, comment_count):
@@ -8,3 +9,25 @@ class SongSnapshot:
         self.likes = likes
         self.dislikes = dislikes
         self.comment_count = comment_count
+
+def get_snapshots(data):
+    snapshots = []
+    for i in range(len(data)):
+        title = data[i]["snippet"]["title"]
+        views = data[i]["statistics"]["viewCount"]
+        likes = data[i]["statistics"]["likeCount"]
+        dislikes = data[i]["statistics"]["dislikeCount"]
+        comment_count = data[i]["statistics"]["commentCount"]
+        snapshot = SongSnapshot(title, views, likes, dislikes, comment_count)
+        snapshots.append(snapshot)
+    return snapshots
+
+def read_youtube_data():
+    days = []
+    for filename in os.listdir("C:\Users\Gijs\PycharmProjects\NetworkDynamics\data\youtube_top100"):
+        data_file = open('../data/youtube_top100/' + filename)
+        data = json.load(data_file)
+        snapshots = get_snapshots(data)
+        days.append(snapshots)
+    return days
+
