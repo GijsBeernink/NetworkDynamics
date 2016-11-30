47c68ff554e2119b59f4f872bc0e52061d1c8ad5
Ron van Bree <ronvbree@gmail.com>
2016-11-30 12:45:47 +0100

    song snapshot

--- misc/util.py
+++ misc/util.py
@@ -1,1 +1,10 @@
 import json
+
+
+class SongSnapshot:
+    def __init__(self, name, views, likes, dislikes, comment_count):
+        self.name = name
+        self.views = views
+        self.likes = likes
+        self.dislikes = dislikes
+        self.comment_count = comment_count
