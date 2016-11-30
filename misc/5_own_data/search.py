91a628d1d2555f4a4929c329ea0870e0d426b779
Ron van Bree <ronvbree@gmail.com>
2016-11-30 11:53:14 +0100

    search verplaatst

--- misc/5_own_data/search.py
+++ misc/5_own_data/search.py
@@ -48,7 +48,7 @@ def youtube_search(options):
 
 
 if __name__ == "__main__":
-  argparser.add_argument("--q", help="Search term", default="Google")
+  argparser.add_argument("--q", help="Search term", default="True Survivor")
   argparser.add_argument("--max-results", help="Max results", default=25)
   args = argparser.parse_args()
 
