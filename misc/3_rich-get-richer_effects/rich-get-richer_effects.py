154b008497b9cadf11294e0603b6c24b57ccac53
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-12-08 23:44:33 +0100

    Begin of barplot

--- misc/3_rich-get-richer_effects/rich-get-richer_effects.py
+++ misc/3_rich-get-richer_effects/rich-get-richer_effects.py
@@ -1,50 +1,76 @@
 import misc.utils.util as util
 import datetime
 import numpy as np
-import scipy.stats as stats
 import matplotlib.pyplot as plt
+from matplotlib import mlab
+import os
+
+path, dirs, files = os.walk("../../data/youtube_top100").next()
+file_count = len(files)
 
 
-# Plots day vs like and day vs dislike of a single song.
-def plot_song(nr, data):
-    view_counts = []
-    title = ""
-    previous_day = -1
+def get_song_views(days_interval, data):
+    songs = dict()
+    datapoints = 0
+    for song in data[0]:
+        songs[song.title] = []
 
-    for day in data:
-        if previous_day == -1:
-            previous_day = day[nr].date - datetime.timedelta(days=1)
-            title = day[nr].title
-            view_counts.append(day[nr].views)
+    for day in range(len(data)):
+        if day % days_interval == 0:
+            datapoints += 1
 
-        elif day[nr].date == previous_day + datetime.timedelta(days=1):
-            view_counts.append(day[nr].views)
-
-        else:
-            view_counts.append(0)
-
-        while day[nr].date > previous_day + datetime.timedelta(days=1):
-            view_counts.append(0)
-            previous_day += datetime.timedelta(days=1)
-
-        previous_day = previous_day + datetime.timedelta(days=1)
-
-    t = np.arange(0, len(view_counts), 1)
-    plt.figure(title)
-    plt.plot(t, view_counts, 'r')
-    plt.ylabel("Views")
-    plt.xlabel(data[0][nr].title)
-    plt.show()
+            for song in data[day]:
+                temp = songs.get(song.title)
+                views = int(song.views)
+                temp.append(views)
+                songs[song.title] = temp
+    return songs
 
 
+# # Plots distribution of the number of views of all songs over several days
+# def plot_distribution(days_interval, data):
+#
+#
+#
+#     x = np.arange(0, file_count, days_interval)
+#
+#     plt.figure("Distribution of songs with" + str(days_interval) + " days interval")
+#
+#     for i in range(0,len(songs.values()[0])):
+#
+#         for song in songs:
+#             if i == 0:
+#                 plt.bar(x, float(songs.get(song.title))/float(totals[i]), 20, color='r')
+#             else:
+#                 plt.bar(x, float(songs.get(song.title))/float(totals[i]), 20, color='y', bottom=songs.get())
+#
+#
+#     #
+#     # plt.bar(x, songs.get(data[0][0].title), 20, color='r')
+#     # plt.bar(x, songs.get(data[0][1].title), 20, color='y', bottom=songs.get(data[0][0].title))
+#     plt.show()
 
-h = [186, 176, 158, 180, 186, 168, 168, 164, 178, 170, 189, 195, 172,
-     187, 180, 186, 185, 168, 179, 178, 183, 179, 170, 175, 186, 159,
-     161, 178, 175, 185, 175, 162, 173, 172, 177, 175, 172, 177, 180]
-h.sort()
-hmean = np.mean(h)
-hstd = np.std(h)
-pdf = stats.norm.pdf(h, hmean, hstd)
-plt.plot(h, pdf) # including h here is crucial
 youtube_data = util.read_youtube_data()
+# plot_distribution(20, youtube_data)
 
+# mu = 100
+# sigma = 25
+# n_bins = 50
+# x = mu + sigma * np.random.randn(10000)
+#
+# n, bins, patches = plt.hist(x, n_bins, normed=1,
+#                             histtype='step', cumulative=True)
+#
+# # Add a line showing the expected distribution.
+# y = mlab.normpdf(bins, mu, sigma).cumsum()
+# y /= y[-1]
+# plt.plot(bins, y, 'k--', linewidth=1.5)
+#
+# # Overlay a reversed cumulative histogram.
+# plt.hist(x, bins=bins, normed=1, histtype='step', cumulative=-1)
+#
+# plt.grid(True)
+# plt.ylim(0, 1.05)
+# plt.title('cumulative step')
+#
+# plt.show()
