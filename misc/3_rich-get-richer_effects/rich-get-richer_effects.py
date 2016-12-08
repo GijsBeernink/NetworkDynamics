a2675c1ce3ddc96e89af56587bb3c3d531cc73e9
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-12-08 20:27:30 +0100

    Rich get richter effects start

--- misc/3_rich-get-richer_effects/rich-get-richer_effects.py
+++ misc/3_rich-get-richer_effects/rich-get-richer_effects.py
@@ -1,10 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import misc.utils.util as util
 import datetime
-
-# Songs to be investigated
-to_investigate = [13, 23, 43, 53, 93]
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 
 # Plots day vs like and day vs dislike of a single song.
@@ -39,7 +37,14 @@ def plot_song(nr, data):
     plt.show()
 
 
+
+h = [186, 176, 158, 180, 186, 168, 168, 164, 178, 170, 189, 195, 172,
+     187, 180, 186, 185, 168, 179, 178, 183, 179, 170, 175, 186, 159,
+     161, 178, 175, 185, 175, 162, 173, 172, 177, 175, 172, 177, 180]
+h.sort()
+hmean = np.mean(h)
+hstd = np.std(h)
+pdf = stats.norm.pdf(h, hmean, hstd)
+plt.plot(h, pdf) # including h here is crucial
 youtube_data = util.read_youtube_data()
 
-for nr in to_investigate:
-    plot_song(nr, youtube_data)
