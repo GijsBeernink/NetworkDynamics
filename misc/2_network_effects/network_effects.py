d00427fa8e7eeb0e2667c39a49838f6100f12ebf
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-12-08 11:52:53 +0100

    Second exercise.
    Added plot for distinct songs.

--- misc/2_network_effects/network_effects.py
+++ misc/2_network_effects/network_effects.py
@@ -3,42 +3,40 @@ import matplotlib.pyplot as plt
 import misc.utils.util as util
 import datetime
 
+
 # Plots day vs like and day vs dislike of a single song.
 def plot_song(nr, data):
     get_song = nr
-    like_counts = []
-    dislike_counts = []
+    view_counts = []
     title = ""
     previous_day = -1
     for day in data:
         if previous_day == -1:
             previous_day = day[get_song].date - datetime.timedelta(days=1)
             title = day[get_song].title
-            like_counts.append(day[get_song].likes)
-            dislike_counts.append(day[get_song].dislikes)
+            view_counts.append(day[get_song].views)
 
         elif day[get_song].date == previous_day + datetime.timedelta(days=1):
-            like_counts.append(day[get_song].likes)
-            dislike_counts.append(day[get_song].dislikes)
+            view_counts.append(day[get_song].views)
 
         else:
-            like_counts.append(0)
-            dislike_counts.append(0)
+            view_counts.append(0)
 
         while day[get_song].date > previous_day + datetime.timedelta(days=1):
-            like_counts.append(0)
-            dislike_counts.append(0)
+            view_counts.append(0)
             previous_day += datetime.timedelta(days=1)
 
         previous_day = previous_day + datetime.timedelta(days=1)
 
-    t = np.arange(0, len(like_counts), 1)
+    t = np.arange(0, len(view_counts), 1)
     plt.figure(title)
-    plt.plot(t, like_counts, 'r', t, dislike_counts, 'b')
-    plt.ylabel("Votes")
+    plt.plot(t, view_counts, 'r')
+    plt.ylabel("Views")
     plt.xlabel("Days since 21-03-2015")
     plt.show()
 
 
 youtube_data = util.read_youtube_data()
+for i in range(len(youtube_data[0])):
+    plot_song(i, youtube_data)
 
