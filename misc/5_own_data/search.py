e57a9d356b1e528bddfb69faf6a403746ed60b32
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-12-09 20:56:31 +0100

    Data collected and plotted

--- misc/5_own_data/search.py
+++ misc/5_own_data/search.py
@@ -4,6 +4,9 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import random as rand
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
@@ -20,7 +23,7 @@ youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
 
 def run(options):
     if options.barry != -1:
-        barry(options)
+        plot(barry(options))
 
     elif options.related != -1:
         find_related(options)
@@ -30,7 +33,7 @@ def run(options):
 
 def find_related(options):
     search_response = youtube.search().list(
-        part="id,snippet",
+        part="id,snippet,statistics",
         maxResults=options.max_results,
         type="video",
         relatedToVideoId=options.related
@@ -42,7 +45,7 @@ def find_related(options):
 def youtube_search(options):
     search_response = youtube.search().list(
         q=options.q,
-        part="id,snippet",
+        part="id,snippet,statistics",
         maxResults=options.max_results
     ).execute()
     videos = handle_response(search_response)
@@ -67,7 +70,7 @@ def handle_response(search_response):
         elif search_result["id"]["kind"] == "youtube#recommendation":
             recommendations[search_result["snippet"]["title"]] = search_result["id"]["videoId"]
 
-    return videos, recommendations
+    return [videos, recommendations]
 
 # def handle_response(search_response):
 #     videos = []
@@ -97,27 +100,51 @@ def handle_response(search_response):
 #     print "Recommendations:\n", "\n".join(recommendations), "\n"
 
 
-# Barry chooses a random video from the recommendations of a random video of the recommendations of another video etc... repeats until Barry found 100 videos.
-
+# Barry chooses a random video from the recommendations of a random video of the recommendations of another video etc...
+# repeats until Barry found 100 videos.
 def barry(options):
     videos_seen = []
+    views = []
 
     search_response = youtube.search().list(
         q=options.q,
         part="id,snippet",
         maxResults=options.max_results
     ).execute()
-    res = handle_response(search_response)
-    videos = res[0]
 
-    video_id = str(videos.get(videos.keys()[0]))
+    videos = dict()
+    recommendations = dict()
+    views = []
+
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos[search_result["snippet"]["title"]] = search_result["id"]["videoId"]
+        elif search_result["id"]["kind"] == "youtube#recommendation":
+            recommendations[search_result["snippet"]["title"]] = search_result["id"]["videoId"]
+
+    for i in range(len(videos)):
+        print i, ": ", videos.keys()[i].encode('ascii', 'ignore')
+
+    var = raw_input("What song?: ")
+
+    video_name = videos.keys()[int(var)].encode('ascii', 'ignore')
+    video_id = str(videos.get(videos.keys()[int(var)]))
+    print "Going with ", video_name
+
     videos_seen.append(video_id)
-    print "Video chosen: ", videos.keys()[0]
 
-    for i in range(100):
+    videos_response = youtube.videos().list(
+        id=video_id,
+        part="statistics",
+        maxResults=options.max_results
+    ).execute()
 
+    views.append(int(videos_response["items"][0]["statistics"]["viewCount"]))
+
+    for i in range(500):
+        print "Busy... (" + str(i) + ")"
         search_response = youtube.search().list(
-            part="id,snippet",
+        part="id,snippet",
             maxResults=options.max_results,
             type="video",
             relatedToVideoId=video_id
@@ -127,17 +154,24 @@ def barry(options):
 
         next = rand.randint(0, len(recommendations) - 1)
         video_id = str(recommendations.get(recommendations.keys()[next]))
-        print recommendations.keys()[next]
+        videos_response = youtube.videos().list(
+            id=video_id,
+            part="statistics",
+            maxResults=options.max_results
+        ).execute()
+
+        videos_seen.append(video_id)
+        views.append(int(videos_response["items"][0]["statistics"]["viewCount"]))
+
+        # print recommendations.keys()[next].encode('ascii', 'ignore')
 
         while video_id in videos_seen:
             other = rand.randint(0, len(recommendations) - 1)
             video_id = str(recommendations.get(recommendations.keys()[other]))
-            print "Other: ", recommendations.keys()[other]
+            # print "Other: ", recommendations.keys()[other].encode('ascii', 'ignore')
 
-        videos_seen.append(video_id)
+    return views
 
-
-    print videos_seen
 
 
     #
@@ -151,12 +185,19 @@ def barry(options):
     # print "Related:\n", "\n".join(recommendations), "\n"
 
 
-
+def plot(data):
+    print data
+    plt.figure("Distribution of views of " + str(len(data)) + " videos")
+    plt.ylabel("Views")
+    plt.xlabel("Nr")
+    x = np.arange(0, len(data), 1)
+    plt.plot(x, sorted(data))
+    plt.show()
 
 
 
 if __name__ == "__main__":
-    argparser.add_argument("--q", help="Search term", default="vrouwkes")
+    argparser.add_argument("--q", help="Search term", default="k3")
     argparser.add_argument("--max-results", help="Max results", default=25)
     argparser.add_argument("--related", help="Related video's to this video ID",default=-1)
     argparser.add_argument("--barry", help="Search video from query and find" +
